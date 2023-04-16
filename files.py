787168e21004aba9b9237963e8f4f1bbd2597902
rinesan <117470993+rinesan@users.noreply.github.com>
2023-04-16 17:53:20 +0200

    leximi i inputeve

--- files.py
+++ files.py
@@ -21,11 +21,20 @@ def encrypt_file(input_file_path, output_file_path, key):
 
     print(f"Encryption successful. Output file written to {output_file_path}")
     def decrypt_file(input_file_path, output_file_path, key):
+
     # Compute the 3DES key from the user-supplied key
     key_hash = hashlib.md5(key.encode('ascii')).digest()
     tdes_key = DES3.adjust_key_parity(key_hash)
+
     # Create a new 3DES cipher object in EAX mode
     cipher = DES3.new(tdes_key, DES3.MODE_EAX)
+
+    # Read the input file into memory
+    with open(input_file_path, 'rb') as input_file:
+        # Read the nonce from the beginning of the file
+        nonce = input_file.read(DES3.block_size)
+        encrypted_bytes = input_file.read()
+        print(f"Encrypted file: {encrypted_bytes}")
 
 
 
