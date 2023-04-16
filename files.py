19d960ca1553c8b3f682a61fd0df5facdf810091
rinesan <117470993+rinesan@users.noreply.github.com>
2023-04-16 17:57:52 +0200

    dekriptimi i bytes

--- files.py
+++ files.py
@@ -22,24 +22,26 @@ def encrypt_file(input_file_path, output_file_path, key):
     print(f"Encryption successful. Output file written to {output_file_path}")
     def decrypt_file(input_file_path, output_file_path, key):
 
-    # Compute the 3DES key from the user-supplied key
+
     key_hash = hashlib.md5(key.encode('ascii')).digest()
     tdes_key = DES3.adjust_key_parity(key_hash)
 
-    # Create a new 3DES cipher object in EAX mode
+
     cipher = DES3.new(tdes_key, DES3.MODE_EAX)
 
-    # Read the input file into memory
+
     with open(input_file_path, 'rb') as input_file:
-        # Read the nonce from the beginning of the file
-        nonce = input_file.read(DES3.block_size)
-        encrypted_bytes = input_file.read()
-        print(f"Encrypted file: {encrypted_bytes}")
+    nonce = input_file.read(DES3.block_size)
+     encrypted_bytes = input_file.read()
+     print(f"Encrypted file: {encrypted_bytes}")
+
     # Decrypt the file bytes
     cipher.nonce = nonce
     decrypted_bytes = cipher.decrypt(encrypted_bytes)
     print(f"Decrypted file: {decrypted_bytes}")
+
     # Write the decrypted bytes to the output file
+
     with open(output_file_path, 'wb') as output_file:
         output_file.write(decrypted_bytes)
     print(f"Decryption successful. Output file written to {output_file_path}")
