cd7abb21eb7aa6b11dc497bdf46b4969bbbccbb3
rinesan <117470993+rinesan@users.noreply.github.com>
2023-04-16 18:15:26 +0200

    dekriptimi i file

--- files.py
+++ files.py
@@ -1,58 +1,51 @@
 import os
 import hashlib
 from Crypto.Cipher import DES3
-
 def encrypt_file(input_file_path, output_file_path, key):
+
     key_hash = hashlib.md5(key.encode('ascii')).digest()
     tdes_key = DES3.adjust_key_parity(key_hash)
 
     cipher = DES3.new(tdes_key, DES3.MODE_EAX, nonce=b'0')
 
     with open(input_file_path, 'rb') as input_file:
-         file_bytes = input_file.read()
-         print(f"input file: {file_bytes}")
+        file_bytes = input_file.read()
+        print(f"Input file: {file_bytes}")
 
     encrypted_bytes = cipher.encrypt(file_bytes)
     print(f"Encrypted file: {encrypted_bytes}")
 
     with open(output_file_path, 'wb') as output_file:
+
         output_file.write(cipher.nonce)
         output_file.write(encrypted_bytes)
-
     print(f"Encryption successful. Output file written to {output_file_path}")
-    def decrypt_file(input_file_path, output_file_path, key):
-
+def decrypt_file(input_file_path, output_file_path, key):
 
     key_hash = hashlib.md5(key.encode('ascii')).digest()
     tdes_key = DES3.adjust_key_parity(key_hash)
 
-
     cipher = DES3.new(tdes_key, DES3.MODE_EAX)
 
-
     with open(input_file_path, 'rb') as input_file:
-    nonce = input_file.read(DES3.block_size)
-     encrypted_bytes = input_file.read()
-     print(f"Encrypted file: {encrypted_bytes}")
 
+        nonce = input_file.read(DES3.block_size)
+        encrypted_bytes = input_file.read()
+        print(f"Encrypted file: {encrypted_bytes}")
 
-
-    # Decrypt the file bytes
     cipher.nonce = nonce
     decrypted_bytes = cipher.decrypt(encrypted_bytes)
     print(f"Decrypted file: {decrypted_bytes}")
-
-    # Write the decrypted bytes to the output file
 
     with open(output_file_path, 'wb') as output_file:
         output_file.write(decrypted_bytes)
     print(f"Decryption successful. Output file written to {output_file_path}")
 
-
 input_file_path = 'input.txt'
 encrypted_file_path = 'encrypted.txt'
-
 decrypted_file_path = 'decrypted.txt'
 key = 'secret'
 
+encrypt_file(input_file_path, encrypted_file_path, key)
 
+decrypt_file(encrypted_file_path, decrypted_file_path, key)
