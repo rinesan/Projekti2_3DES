43fcc502253ca69570ae7909a227fde432a40863
RinesaHoxha <rinesa.hoxha1@student.uni-pr.edu>
2023-04-16 16:38:32 +0200

    inicializimi i instances

--- files.py
+++ files.py
@@ -6,3 +6,5 @@ def encrypt_file(input_file_path, output_file_path, key):
     key_hash = hashlib.md5(key.encode('ascii')).digest()
     tdes_key = DES3.adjust_key_parity(key_hash)
 
+    cipher = DES3.new(tdes_key, DES3.MODE_EAX, nonce=b'0')
+
