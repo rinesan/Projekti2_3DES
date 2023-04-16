79dfabdcffb6787b8ba5dcc219fa8bc7ef2e359e
RinesaHoxha <rinesa.hoxha1@student.uni-pr.edu>
2023-04-16 16:35:16 +0200

    inputi i file dhe key

--- files.py
+++ files.py
@@ -2,4 +2,7 @@ import os
 import hashlib
 from Crypto.Cipher import DES3
 
+def encrypt_file(input_file_path, output_file_path, key):
+    key_hash = hashlib.md5(key.encode('ascii')).digest()
+    tdes_key = DES3.adjust_key_parity(key_hash)
 
