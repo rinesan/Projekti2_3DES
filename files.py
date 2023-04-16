290a516b1cd0b856feb03581b447c1384a9bf8ea
RinesaHoxha <rinesa.hoxha1@student.uni-pr.edu>
2023-04-16 15:16:45 +0200

    inputi i file

--- files.py
+++ files.py
@@ -1,1 +1,10 @@
 from Crypto.Cipher import DES3
+from hashlib import md5
+
+while True:
+    print('Choose one of the following operations: \n\t1- encrypt\n\t2- decrypt')
+    operation = input("Your choice: ")
+    if operation not in['1','2']:
+        break
+        file_path=input('filepath:')
+        key = input('tdes key:')
